ef8a10f057978ab958f30bf01dbfe518acad3a76
resuryasin <resuryasin.developer@gmail.com>
2023-07-11 23:42:03 +0700

    re: Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+***Test***
+**build**
+*pkg*
+
+minimalist
---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+from setuptools import setup
+
+setup(name="package_name",
+      version="0.1",
+      description="Hello World",
+      url="http://github.com/resuryasin/pkgtest",
+      author="resuryasin",
+      author_email="resuryasin.developer@gmail.com",
+      license="None",
+      packages=["package_name"],
+      zip_safe=False)
